8cdae45c6389172d957c2f0120b8254fc7ead264
PrateekSharma007 <btech.m2_66995@apsc.mait.ac.in>
2025-08-03 03:02:06 +0530

    Initial commit

---
+++ userService/src/main/java/com/fitness/userService/model/User.java
@@ -0,0 +1,34 @@
+package com.fitness.userService.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+    private String firstName;
+    private String lastName;
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAtt;
+
+}
