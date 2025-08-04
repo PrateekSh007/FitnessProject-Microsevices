61e3889ac03e4b228f7aa68966da384a4e1e47b9
PrateekSharma007 <btech.m2_66995@apsc.mait.ac.in>
2025-08-04 23:32:52 +0530

    activityService

--- activityService/src/main/java/com/fitness/activityService/service/ActivityService.java
+++ activityService/src/main/java/com/fitness/activityService/service/ActivityService.java
@@ -2,6 +2,7 @@ package com.fitness.activityService.service;
 
 import com.fitness.activityService.dto.ActivityRequest;
 import com.fitness.activityService.dto.ActivityResponse;
+import com.fitness.activityService.model.Activity;
 import com.fitness.activityService.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,29 @@ public class ActivityService {
     private ActivityRepository activityRepository;
 
     public ActivityResponse trackActivity(ActivityRequest activityRequest) {
+        Activity activity = Activity.builder()
+                .userId(activityRequest.getUserId())
+                .activity(activityRequest.getActivityType())
+                .duration(activityRequest.getDuration())
+                .caloriesBurned(activityRequest.getCaloriesBurned())
+                .startTime(activityRequest.getStartTime())
+                .additionalMetrics(activityRequest.getAdditionalMetrics())
+                .build() ;
+
+        Activity savedActivity = activityRepository.save(activity) ;
+
+        ActivityResponse activityResponse =  new ActivityResponse()
+        activityResponse.setUserId(savedActivity.getUserId());
+        activityResponse.setId(savedActivity.getId());
+        activityResponse.setActivity(savedActivity.getActivity());
+        activityResponse.setDuration(savedActivity.getDuration());
+        activityResponse.setCaloriesBurned(savedActivity.getCaloriesBurned());
+        activityResponse.setStartTime(savedActivity.getStartTime());
+        activityResponse.setCreatedAt(savedActivity.getCreatedAt());
+        activityResponse.setUpdatedAt(savedActivity.getUpdatedAt());
+        activityResponse.setAdditionalMetrics(savedActivity.getAdditionalMetrics());
+
+        return activityResponse;
 
     }
 }
